31a935fa2025705d4c068ffe24da8a39f5b9ee76
oliverJBW <oliver_bw@hotmail.com>
2023-01-13 16:00:18 +0100

    aviso arreglado

---
+++ cache/75466332778f095a6bfbb9a5f852c9d5a8479154.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plantilla básica</title>
+</head>
+<body>
+    <h1>Plantilla básica</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nam hic est aspernatur a molestiae doloribus quod esse ratione error quos, vel harum voluptas facilis nesciunt saepe fuga explicabo sunt?</p>
+</body>
+</html>
